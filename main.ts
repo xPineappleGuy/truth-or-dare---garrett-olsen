3f73c2fc1c7bb7e3e19a0406230313c51148d88a
xPineappleGuy <112982283+xPineappleGuy@users.noreply.github.com>
2022-09-13 12:59:46 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,63 @@
+input.onButtonPressed(Button.A, function () {
+    Truth = randint(0, 1)
+    if (Truth == 1) {
+        basic.showString("Truth")
+    } else {
+        basic.showString("Dare")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Dare = randint(0, 3)
+    if (Dare == 0) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (Truth == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (Truth == 2) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+let Dare = 0
+let Truth = 0
+basic.showString("Truth Or Dare")
+basic.forever(function () {
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+})
